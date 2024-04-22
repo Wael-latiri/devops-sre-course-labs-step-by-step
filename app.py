a8d1eb1d3703004be151913f2ed7688cd2205c30
unknown <wael6latiri@gmail.com>
2024-04-22 11:58:24 +0100

    fixed/added workflow

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-"""
-
-"""
+"""This module contains the main application logic."""
 
 from flask import Flask, request, render_template
 
 
-APP = Flask(__name__,template_folder='templates')
+APP = Flask(__name__, template_folder='templates')
 
 
 @APP.route('/')
@@ -17,15 +15,17 @@ def home():
     return render_template('index.html')
 
 
-@APP.route('/hello',methods=['POST'])
+@APP.route('/hello', methods=['POST'])
 def hello():
     '''
     For rendering results on HTML GUI
     '''
-    inputs = [x for x in request.form.values()]
-    result = " Hello There " + inputs[0]
-    return render_template('index.html', hello_text='{}'.format(result))
+    list(request.form.values())
+
+    inputs = list(request.form.values())  # Adjusted line
+    result = f"Hello There {inputs[0]}"  # Adjusted line
+    return render_template('index.html', hello_text=result)  # Adjusted line
 
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0',port=8080)
+    APP.run(host='0.0.0.0', port=8080)
